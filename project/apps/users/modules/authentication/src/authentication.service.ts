ad26173233a34063bd0d5a27929a296875bb2be3
alaltru1992 <33177697+alaltru1992@users.noreply.github.com>
2025-01-01 23:48:39 +0300

    module2-task1

--- project/apps/users/modules/authentication/src/authentication.service.ts
+++ project/apps/users/modules/authentication/src/authentication.service.ts
@@ -1,8 +1,28 @@
-import { Injectable } from '@nestjs/common';
-import {UserRepository} from "@project/user";
+import { Injectable, ConflictException } from '@nestjs/common';
+import {UserRepository, UserEntity} from "@project/user";
+import {CreateUser} from "../src";
+import dayjs from 'dayjs';
+import {UserRole} from "@project/core";
 
-@Injectable()
 export class AuthenticationService {
-  constructor(userRepository: UserRepository) {
+  constructor(private readonly userRepository: UserRepository) {
+  }
+
+  public async register(dto: CreateUser){
+
+    const {firstName, dateBirth, lastName, email, password } = dto;
+
+    const currentUser = {
+      firstName, dateOfBirth: dayjs(dateBirth).toDate(), lastName, email, role: UserRole.User, hashPassWord: ''
+    }
+
+    const doesUserExist = await this.userRepository.findUserByEmail(email)
+     if(!doesUserExist){
+       throw new ConflictException('Пользователь существует');
+     }
+
+     const userEntity = await new UserEntity(currentUser).setPASSword(password);
+
+     return this.userRepository.save(userEntity)
   }
 }
